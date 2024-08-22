1625ba07e69f3bea449e67f25fb663331e32ea1d
Mayank Gupta <mg919889@gmail.com>
2024-08-23 01:38:48 +0530

    Validation

---
+++ src/Users/userController.ts
@@ -0,0 +1,18 @@
+import { Request,Response,NextFunction } from "express";
+import createHttpError from "http-errors";
+
+const createUser=async(req:Request,res:Response,next:NextFunction)=>{
+    const {name,email,password}=req.body;
+    //Validation
+    if(!name || !email ||!password){
+        const error=createHttpError(400,"All fields are required");
+
+        return next(error)
+    }
+
+
+    res.json({message:"User Created"});
+}
+
+
+export {createUser};
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
-import express from 'express'
+import express from 'express';
 import globalErrorHandler from './middleware/globalErrorHandler';
+import userRouter from './Users/userRouter';
 
-const app=express();
+const app = express();
 
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.json({message:"hey"})
-})
+app.get('/', (req, res) => {
+    res.json({message: "hey"});
+});
 
+app.use("/api/users",userRouter)
 
 app.use(globalErrorHandler);
-
-
-
-
-
-
-
 
 export default app;
